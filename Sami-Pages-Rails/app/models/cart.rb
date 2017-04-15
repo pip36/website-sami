d094585982b0d1aaa8e870ffe159bae023dac23c
Philip White <phil_23@live.co.uk>
2017-04-15 10:00:11 +0100

    partial paypal integration

--- Sami-Pages-Rails/app/models/cart.rb
+++ Sami-Pages-Rails/app/models/cart.rb
@@ -1,4 +1,12 @@
 class Cart < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
+
+  def total
+    @sum = 0
+    order_items.each do |order|
+      @sum += order.subtotal
+    end
+    return @sum
+  end
 end
