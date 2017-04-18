879bef4591e0c329ebf21ce44459fe09b24b2906
Philip White <phil_23@live.co.uk>
2017-04-18 13:08:39 +0100

    added seed for product data

--- Sami-Pages-Rails/db/seeds.rb
+++ Sami-Pages-Rails/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+  {name: 'Rouche', unit_price: '1.80'},
+  {name: 'Neuranter', unit_price: '1.80'}
+  ])
