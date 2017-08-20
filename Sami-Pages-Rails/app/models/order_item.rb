d2a9a47b013b3107fc37626d58379c4d1e15e8eb
pip36 <phil_23@live.co.uk>
2017-08-20 09:48:15 +0100

    added stock validation

--- Sami-Pages-Rails/app/models/order_item.rb
+++ Sami-Pages-Rails/app/models/order_item.rb
@@ -4,8 +4,15 @@ class OrderItem < ApplicationRecord
 
   validates :product_id, presence: true
   validates :quantity, presence: true, numericality: {greater_than: 0}
+  validate :product_in_stock
 
   def subtotal
     @subtotal = product.unit_price * quantity
+  end
+
+  def product_in_stock
+    if product.stock < 1
+      self.errors.add(:order_item, "Product is out of stock")
+    end
   end
 end
